d0548c25977f0ddcaaf26d52a618a6f7453f00f8
Nishant Tirkey <nishant.t123@gmail.com>
2022-09-20 14:16:59 +0530

    Day2 code completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 1;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Catalog App')),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("This is my first day out of $days of learning Flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
